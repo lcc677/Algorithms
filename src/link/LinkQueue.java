44bc5a8af42befb6e524b40c7b58dbe770c188ef
liuchenchen <l_cc677@163.com>
2021-11-17 22:26:48 +0800

    双向链表队列

--- src/link/LinkQueue.java
+++ src/link/LinkQueue.java
@@ -62,45 +62,4 @@ class Queue{
 			end.setNext(data);
 		}
 	}
-	
-	
-}
-
-class Data{
-	private String name;
-	private int no;
-	private Data next;
-	
-	public Data(String name, int no) {
-		this.name = name;
-		this.no = no;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getNo() {
-		return no;
-	}
-	public void setNo(int no) {
-		this.no = no;
-	}
-	public Data getNext() {
-		return next;
-	}
-	public void setNext(Data next) {
-		this.next = next;
-	}
-
-	@Override
-	public String toString() {
-		return this.no + "";
-	}
-	
-	
-	
-	
 }
